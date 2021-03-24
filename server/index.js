00e7423ba2454a957fc68172264c19a705cdec8f
Sabir Kirpal <skirpal2@ucmerced.edu>
2021-03-24 11:38:39 -0700

    adding gitignore

--- server/index.js
+++ server/index.js
@@ -11,19 +11,19 @@ const tasks = require("./api/tasks.js");
 app.use(cors());
 app.use(volleyball);
 app.use(express.json());
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
 });
 
 app.get('/', (req, res) => {
-    res.json({
-        message: "La Hacks Server",
+  res.json({
+    message: "La Hacks Server",
 
-    });
+  });
 });
 
 app.use("/auth", auth);
@@ -31,16 +31,16 @@ app.use("/auth", auth);
 app.use("/tasks", middleware.isLoggedin, tasks);
 
 app.listen(8081, () => {
-    console.log("Listening at http://localhost:8081");
+  console.log("Listening at http://localhost:8081");
 });
 
 function errorHandler(error, req, res, next) {
-    res.status(res.statusCode || 500);
-    res.json({
-        message: error.message,
-        stack: error.stack,
-    });
-    res.end();
+  res.status(res.statusCode || 500);
+  res.json({
+    message: error.message,
+    stack: error.stack,
+  });
+  res.end();
 }
 
 app.use(errorHandler);
